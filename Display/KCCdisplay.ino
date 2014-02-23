a49272122ab6fafd1e1336026eed08483ed55726
Gabriel Fornaeus <gf@hax0r.se>
2014-02-23 20:20:21 +0100

    SPI mode instead then

--- Display/KCCdisplay.ino
+++ Display/KCCdisplay.ino
@@ -2,64 +2,68 @@
  * File Name :
  * Purpose :
  * Creation Date :
- * Last Modified : sön 23 feb 2014 18:00:01
+ * Last Modified : sön 23 feb 2014 20:20:18
  * Created By : Gabriel Fornaeus, <gf@hax0r.se>
  *
  */
 
-// MAX7219-controlled display library
-#include <LedControl.h>
+ // define max7219 registers
+ const byte MAX7219_REG_NOOP        = 0x0;
+ const byte MAX7219_REG_DIGIT0      = 0x1;
+ const byte MAX7219_REG_DIGIT1      = 0x2;
+ const byte MAX7219_REG_DIGIT2      = 0x3;
+ const byte MAX7219_REG_DIGIT3      = 0x4;
+ const byte MAX7219_REG_DIGIT4      = 0x5;
+ const byte MAX7219_REG_DIGIT5      = 0x6;
+ const byte MAX7219_REG_DIGIT6      = 0x7;
+ const byte MAX7219_REG_DIGIT7      = 0x8;
+ const byte MAX7219_REG_DECODEMODE  = 0x9;
+ const byte MAX7219_REG_INTENSITY   = 0xA;
+ const byte MAX7219_REG_SCANLIMIT   = 0xB;
+ const byte MAX7219_REG_SHUTDOWN    = 0xC;
+ const byte MAX7219_REG_DISPLAYTEST = 0xF;
 
-LedControl altitudeDisplay = LedControl(8,9,10,1);
+#include <SPI.h>
+
+void sendByte(const byte reg, const byte data) {
+	SPI.transfer(reg);
+	SPI.transfer(data);
+}
+
+void showNumber(const int num) {
+	char buf[8];
+	sprintf(buf, "%7l", min(max(num,0), 9999999));
+	for(byte digit=0;digit < 7; digit++) {
+		byte c = buf[digit];
+		if(c == ' ')
+			c = 0xF; // Code for a blank
+		else
+			c -= '0';
+		sendByte(digit +1, c);
+	}
+}
+
 void setup() {
 	Serial.begin(9600);
-
-	// Initialize display
-	// Enable display
-	altitudeDisplay.shutdown(0, false);
-	// Set brightness
-	altitudeDisplay.setIntensity(0,5);
-	// Clear the display register
-	altitudeDisplay.clearDisplay(0);
+	SPI.begin();
+	
+	// Max 7 digits
+	sendByte(MAX7219_REG_SCANLIMIT, 7);
+	// Use digits, no bit pattern
+	sendByte(MAX7219_REG_DECODEMODE, 0xFF);
+	// Don't test the display first
+	sendByte(MAX7219_REG_DISPLAYTEST, 0);
+	
+	for(byte col=0; col < 8; col++)
+		sendByte(col+1,0);
+	// Intensity at ~50%
+	sendByte(MAX7219_REG_INTENSITY, 7);
+	// Start the display
+	sendByte(MAX7219_REG_SHUTDOWN, 1);
 }
 
-void printNumber(int v) {
-	int ones;
-	int tens;
-	int hundreds;
-	int thousands;
-	int tenthousands;
-	bool negative;
-
-	if(v<0) {
-		negative=true;
-		v= v*-1;
-	}
-	// Convert in to groups
-	ones = v%10;
-	v=v/10;
-
-	tens=v%10;
-	v=v/10;
-
-	hundreds=v%10;
-	v=v/10;
-
-	if(negative) {
-		altitudeDisplay.setChar(0,7,'-',false);
-	} else {
-		altitudeDisplay.setChar(0,7,0,false);
-	}
-
-	altitudeDisplay.setDigit(0,2,hundreds,false);
-	altitudeDisplay.setDigit(0,1,tens,false);
-	altitudeDisplay.setDigit(0,0,ones,false);
-}
-
+unsigned int i;
 void loop() {
-	// Just a simple counter for now
-	for(int i=-999; i<999;i++) {
-		printNumber(i);
-		delay(10);
-	}
+	showNumber(i++);
+	delay(300);
 }
