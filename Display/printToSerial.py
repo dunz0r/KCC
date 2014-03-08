7012f76f4696a010c303a3ef5a9cf3571e2b95c2
Gabriel Fornaeus <gf@hax0r.se>
2014-03-08 21:52:51 +0100

    Added the pythonprogram which prints to display

---
+++ Display/printToSerial.py
@@ -0,0 +1,26 @@
+import serial
+import requests
+import re
+import time
+
+url = 'http://localhost:8085/telemachus/datalink?a=v.altitude'
+ser = serial.Serial('/dev/ttyACM0', 38400, timeout = 1)
+
+def toSerial(value):
+    ser.write(value)
+
+def getContent():
+    r = requests.get(url)
+    output = re.sub('[{}:a"]+', '',r.content)
+    return output
+
+def formatData(data):
+    output = str(round(float(data),1))
+    return output
+
+while(1>0):
+    content = getContent()
+    content = formatData(content)
+    print(re.sub('\..', 'S', content))
+    toSerial(re.sub('\..', 'S', content))
+    time.sleep(0.05)
