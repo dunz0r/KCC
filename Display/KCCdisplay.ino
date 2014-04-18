83f681b66f7fe1c64e144c30dbf4a485d89a08b6
Gabriel Fornaeus <gf@hax0r.se>
2014-04-19 01:42:17 +0200

    Decimal places, apoapsis and optimisation

--- Display/KCCdisplay.ino
+++ Display/KCCdisplay.ino
@@ -2,21 +2,36 @@
  * File Name :
  * Purpose :
  * Creation Date :
- * Last Modified : fre 18 apr 2014 23:17:34
+ * Last Modified : lör 19 apr 2014 01:33:54
  * Created By : Gabriel Fornaeus, <gf@hax0r.se>
  *
  */
 
 
-// For controlling the display
-#include "LedControl.h"
-// 12->DATA IN, 11-> CLK, 10-> LOAD/CS, number of displays
-LedControl display=LedControl(12,11,10,2);
-
 // This switch is connected to three resistors, giving three different modes
 #define VEL_SWITCH A0
 
-void printDecimalNumber(uint32_t v, int address) {
+// Number of displays
+#define NUM_DISPLAYS 3
+// For controlling the display
+#include "LedControl.h"
+// 12->DATA IN, 11-> CLK, 10-> LOAD/CS, number of displays
+LedControl display=LedControl(12,11,10,NUM_DISPLAYS);
+
+void setup() {
+
+	// Enable the display
+	for(unsigned int i=0;i < NUM_DISPLAYS;i++){
+		display.shutdown(i,false);
+		display.setIntensity(i,4);
+		display.clearDisplay(i);
+	}
+
+	Serial.begin(38400);
+}
+
+
+void printDecimalNumber(uint32_t v, int address, bool decimalPoint) {
 	int onDisplay[8];
 
 	for(unsigned int i=0;i<sizeof(onDisplay);i++){
@@ -25,25 +40,10 @@ void printDecimalNumber(uint32_t v, int address) {
 		// The first character printed will be the decimal number, so put the decimal point 
 		// so put the decimal point on the second character
 		if(i == 1)
-			display.setDigit(address,i,(byte)onDisplay[i],true);
+			display.setDigit(address,i,(byte)onDisplay[i],decimalPoint);
 		else if( i != 1)
 			display.setDigit(address,i,(byte)onDisplay[i],false);
 	}
-}
-
-void setup() {
-
-	// Enable the display
-	display.shutdown(0,false);
-	display.shutdown(1,false);
-	// Set brightness
-	display.setIntensity(0,4);
-	display.setIntensity(1,4);
-	// Clear the display
-	display.clearDisplay(0);
-	display.clearDisplay(1);
-
-	Serial.begin(38400);
 }
 
 int getFuel() {
@@ -72,7 +72,7 @@ int getCharge() {
 	return (int)(percent*255);
 }
 
-uint32_t getVelocity(short mode) {
+uint32_t getVelocity(uint8_t mode) {
 	uint32_t velocity;
 
 	switch(mode) {
@@ -89,36 +89,46 @@ uint32_t getVelocity(short mode) {
 			Serial.print("sV");
 			velocity = Serial.parseInt();
 	}
-	return velocity/10;
+	return velocity;
 }
 
+uint32_t getApoapsis() {
+	uint32_t apoapsis;
+	Serial.print("ap");
+	apoapsis = Serial.parseInt();
+	// Divide by 10 to get rid of the decimal
+	return apoapsis/10;
+}
 uint32_t getAltitude() {
 	uint32_t altitude;
 
 	Serial.print("al");
 	altitude = Serial.parseInt();
-	return altitude;
+	// Divide by 10 to get rid of the decimal
+	return altitude/10;
 }
 
-short getVelocityMode() {
+uint8_t getVelocityMode() {
 	// This is what gets returned
-	short mode = 0;
-	int buttonReading = analogRead(VEL_SWITCH);
+	uint8_t mode = 0;
+	int switchReading = analogRead(VEL_SWITCH);
 
-	if(VEL_SWITCH > 600)
+	if(switchReading > 600)
 		mode = 0;
 	else if((VEL_SWITCH < 600) && (VEL_SWITCH > 300))
 		mode = 1;
 	else
 		mode = 2;
+	return mode;
 }
 
 void loop() {
 	int chargePercent = getCharge();
 	int fuelPercent = getFuel();
-	velocityMode = getVelocityMode();
+	uint8_t velocityMode = getVelocityMode();
 	analogWrite(6,chargePercent);
 	analogWrite(3,fuelPercent);
-	printDecimalNumber(getAltitude(), 1);
-	printDecimalNumber(getVelocity(velocityMode), 0);
+	printDecimalNumber(getApoapsis(), 2, false);
+	printDecimalNumber(getAltitude(), 1, false);
+	printDecimalNumber(getVelocity(velocityMode), 0, true);
 }
