908e4807c0020854da81dad339bbc7150367330d
Gabriel Fornaeus <gf@hax0r.se>
2014-04-18 22:14:01 +0200

    Decimal points

--- Display/printToSerial.py
+++ Display/printToSerial.py
@@ -4,22 +4,27 @@ import json
 import time
 
 url = 'http://localhost:8085/telemachus/datalink?al=v.altitude&ve=v.orbitalVelocity&cM=r.resourceMax[ElectricCharge]&cC=r.resource[ElectricCharge]&fC=r.resource[fuel]&fM=r.resourceMax[fuel]'
-ser = serial.Serial('/dev/ttyACM0', 38400, timeout = 1)
+ser = serial.Serial('/dev/ttyACM1', 38400, timeout = 1)
 
 def toSerial(value):
     ser.write(value)
 
 def getContent():
-    r = requests.get(url)
-    return r.content
+    try:
+        r = requests.get(url)
+        return r.content
+    except ValueError:
+        pass
 
-def formatData(data):
-    output = str(int(data)) + 's'
+def formatNumber(data):
+    data = round(data,1)
+    # To get decimal numbers on the LED-displays
+    output = str(int(data*10) + 's'
     return output
 
 while(1>0):
     content = getContent()
     content = json.loads(content)
     dataRequested = ser.read(2)
-    toSerial(formatData(content[dataRequested]))
+    toSerial(formatNumber(content[dataRequested]))
     time.sleep(0.01)
